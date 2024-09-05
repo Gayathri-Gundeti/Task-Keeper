357c5ff26f70db20ea597c77a7146e0ed854b838
Gayathri-Gundeti <gundetigayathri0502@gmail.com>
2024-09-05 12:00:50 +0530

    adding vercel.json file

--- server/api.js
+++ server/api.js
@@ -1,154 +1,93 @@
-// var express = require("express");
-// var cors = require("cors");
-// var mongoClient = require("mongodb").MongoClient;
+// api/index.js
 
-// var conString = "mongodb+srv://gundetigayathri0502:gayathri@todo-list.7kkxt.mongodb.net/";
-// var app = express();
-// app.use(cors());
+require("dotenv").config({path:"../.env"}); // Load environment variables from .env file
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
 
-// app.get("/get-items",(req,res)=>{
-//          mongoClient.connect(conString).then(clientObject=>{
-//              var database=clientObject.db("todo-list");
-//              database.collection("items").find({}).toArray().then(documents=>{
-//                  res.send(documents);
-//                  res.end();
-//              });
-//          });
-//     });
-// app.get("/get-items/:id",(req,res)=>{
-//     mongoClient.connect(conString).then(clientObject=>{
-//         var database=clientObject.db("todo-list");
-//         database.collection("items").find({Id:parseInt(req.params.id)}).toArray().then(documents=>{
-//             res.send(documents);
-//             res.end();
-//         })
-//     })
-// })
-
-// app.post("/list-items", (req, res) => {
-//     var list = {
-//         Id:parseInt(req.body.Id),
-//         Item: req.body.Item,
-//     }
-//     mongoClient.connect(conString).then(clientObject => {
-//         var database = clientObject.db("todo-list");
-//         database.collection("items").insertOne(list).then(() => {
-//             console.log("Item Added");
-//             res.end();
-//         })
-//     })
-// })
-// app.put("/update/:id",(req,res)=>{
-//     var id=parseInt(req.params.id);
-//     var list={
-//         Id:parseInt(req.body.Id),
-//         Item:req.body.Item
-//     };
-//    mongoClient.connect(conString).then(clientObject=>{
-//     var database=clientObject.db("todo-list");
-//     database.collection("items").updateOne({Id:id},{$set:list}).then(()=>{
-//         console.log("Item updated..");
-//         res.end();
-//     });
-//    });
-
-// });
-// app.delete("/delete/:id",(req,res)=>{
-//     var id=parseInt(req.params.id);
-//     mongoClient.connect(conString).then(clientObject=>{
-//         var database=clientObject.db("todo-list");
-//         database.collection("items").deleteOne({Id:id}).then(()=>{
-//             console.log("Item deleted..");
-//             res.end();
-//         });
-
-//     });
-
-// });
-// app.listen(7700);
-// console.log(`Server started: http://127.0.0.1:7700`);
-
-
-require("dotenv").config(({ path: "../.env" })); // Load environment variables from .env file
-var express = require("express");
-var cors = require("cors");
-var mongoClient = require("mongodb").MongoClient;
-
-var conString = process.env.MONGO_URI; // Use environment variable
-var app = express();
+const conString = process.env.MONGO_URI; // Use environment variable
+const app = express();
 app.use(cors());
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/get-items", (req, res) => {
-    mongoClient.connect(conString).then(clientObject => {
-        var database = clientObject.db(process.env.DB_NAME); // Use environment variable
-        database.collection("items").find({}).toArray().then(documents => {
-            res.send(documents);
-            res.end();
-        });
-    }).catch(err => console.error(err));
+// Endpoint: Get all items
+app.get("/api/get-items", async (req, res) => {
+  try {
+    const clientObject = await MongoClient.connect(conString);
+    const database = clientObject.db(process.env.DB_NAME);
+    const documents = await database.collection("items").find({}).toArray();
+    res.send(documents);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching items");
+  }
 });
 
-app.get("/get-items/:id", (req, res) => {
-    mongoClient.connect(conString).then(clientObject => {
-        var database = clientObject.db(process.env.DB_NAME); // Use environment variable
-        database.collection("items").find({ Id: parseInt(req.params.id) }).toArray().then(documents => {
-            res.send(documents);
-            res.end();
-        });
-    }).catch(err => console.error(err));
+// Endpoint: Get item by ID
+app.get("/api/get-items/:id", async (req, res) => {
+  try {
+    const clientObject = await MongoClient.connect(conString);
+    const database = clientObject.db(process.env.DB_NAME);
+    const documents = await database.collection("items").find({ Id: parseInt(req.params.id) }).toArray();
+    res.send(documents);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching item by ID");
+  }
 });
 
-app.post("/list-items", (req, res) => {
-    var list = {
-        Id: parseInt(req.body.Id),
-        Item: req.body.Item,
+// Endpoint: Add a new item
+app.post("/api/list-items", async (req, res) => {
+  try {
+    const list = {
+      Id: parseInt(req.body.Id),
+      Item: req.body.Item,
     };
-    mongoClient.connect(conString).then(clientObject => {
-        var database = clientObject.db(process.env.DB_NAME); // Use environment variable
-        database.collection("items").insertOne(list).then(() => {
-            console.log("Item Added");
-            res.end();
-        });
-    }).catch(err => console.error(err));
+    const clientObject = await MongoClient.connect(conString);
+    const database = clientObject.db(process.env.DB_NAME);
+    await database.collection("items").insertOne(list);
+    console.log("Item Added");
+    res.status(201).send("Item added successfully");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error adding item");
+  }
 });
 
-app.put("/update/:id", (req, res) => {
-    var id = parseInt(req.params.id);
-    var list = {
-        Id: parseInt(req.body.Id),
-        Item: req.body.Item
+// Endpoint: Update an item by ID
+app.put("/api/update/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const list = {
+      Id: parseInt(req.body.Id),
+      Item: req.body.Item
     };
-    mongoClient.connect(conString).then(clientObject => {
-        var database = clientObject.db(process.env.DB_NAME); // Use environment variable
-        database.collection("items").updateOne({ Id: id }, { $set: list }).then(() => {
-            console.log("Item updated..");
-            res.end();
-        });
-    }).catch(err => console.error(err));
+    const clientObject = await MongoClient.connect(conString);
+    const database = clientObject.db(process.env.DB_NAME);
+    await database.collection("items").updateOne({ Id: id }, { $set: list });
+    console.log("Item updated..");
+    res.status(200).send("Item updated successfully");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error updating item");
+  }
 });
 
-app.delete("/delete/:id", (req, res) => {
-    var id = parseInt(req.params.id);
-    mongoClient.connect(conString).then(clientObject => {
-        var database = clientObject.db(process.env.DB_NAME); // Use environment variable
-        database.collection("items").deleteOne({ Id: id }).then(() => {
-            console.log("Item deleted..");
-            res.end();
-        });
-    }).catch(err => console.error(err));
+// Endpoint: Delete an item by ID
+app.delete("/api/delete/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const clientObject = await MongoClient.connect(conString);
+    const database = clientObject.db(process.env.DB_NAME);
+    await database.collection("items").deleteOne({ Id: id });
+    console.log("Item deleted..");
+    res.status(200).send("Item deleted successfully");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error deleting item");
+  }
 });
 
-console.log(process.env.PORT)
-const PORT = process.env.PORT || 3000; // Use environment variable
-app.listen(PORT, () => {
-    console.log(`Server started: http://127.0.0.1:${PORT}`);
-});
-
-
-
+module.exports = app;
